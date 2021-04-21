e1abe0b9da736db66a5dc4405bc3031b07f89fbe
saipavani68 <nagisettipavani@gmail.com>
2021-04-20 21:11:17 -0700

    handled empty cart scenario

--- src/components/cart-page.jsx
+++ src/components/cart-page.jsx
@@ -5,19 +5,22 @@ import './product.css';
 class CartPage extends Component {
     render () {
         const { items } = this.props
-        console.log(items);
         return(
             <div className="cart-items-container">
                 <h2 className="cart-items-header"> Shopping Cart </h2>
-                { items.length > 0 && items.map(item => (
+                { items.length > 0 ? items.map(item => (
                         <div className="cart-item-content" key={ item[0].id }> 
                             <img src={ item[0].image_name } alt="item image"  className="cart-item-image" />
                             <div className="cart-item-title">{ item[0].title }, { item[0].description }  </div>
                             <div className="cart-item-price"> ${ item[0].price } </div>
                         </div>
                     ))
+                    :
+                    <div className="cart-item-content cart-empty-container"> Your Cart is empty. 
+                        <a href="/products"> Continue Shopping </a> 
+                    </div>
                 }
-                <h5 className="cart-total-price">Subtotal ({items.length}): { items.flat().reduce((a, b) => a + +b.price, 0).toFixed(2)} </h5>
+                { items.length > 0 && <h5 className="cart-total-price">Subtotal ({items.length}): { items.flat().reduce((a, b) => a + +b.price, 0).toFixed(2)} </h5> }
             </div>
         );
     }
